d5eb99c2c2bec42064f1a22fa97fcc26dd16823d
Antoine Magnin <antonius.manianus@icloud.com>
2017-05-10 22:41:06 +0200

    feat(client): index-search component improvement
    Check if index exists before broadcasting.

--- src/client/components/index-search/index-search.component.js
+++ src/client/components/index-search/index-search.component.js
@@ -2,15 +2,24 @@ angular.module('indexSearch').component('indexSearch', {
 
     templateUrl: 'components/index-search/index-search.template.html',
 
-    controller: ['$rootScope', function IndexSearchController($rootScope) {
+    controller: ['$rootScope', '$http', function IndexSearchController($rootScope, $http) {
 
         var self = this;
         this.index = null;
+        this.error = null;
 
         this.search = function () {
             if (this.index) {
-                $rootScope.$broadcast('index', this.index);
-                this.index = null;
+                $http({
+                    method: 'GET',
+                    url: 'http://localhost:8080/index/' + this.index
+                }).then(function successCallback(response) {
+                    if (response.data.valid) { 
+                        $rootScope.$broadcast('index', self.index);
+                        self.error = null;
+                        self.index = null;
+                    } else self.error = 'Index doesn\'t exists';
+                });
             }
         };
 
