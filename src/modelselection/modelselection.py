964baca7f4e0e7902e8ec458b24368f56b004683
Rudolf Höhn <rudolf.hohn@gmail.com>
2017-05-15 09:19:58 +0200

    feat(script): working script for server processing
    Add the script file used to process the model selection on a distant
    server (AWS)
    Add gitignore entry for pickled files

--- src/modelselection/modelselection.py
+++ src/modelselection/modelselection.py
@@ -2,6 +2,15 @@ import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
 from sklearn.model_selection import GridSearchCV, train_test_split
+from sklearn import svm
+import pickle
+
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+
+from sklearn.utils import shuffle
+
+print('PROGRAM STARTED')
 
 # self-drive
 df_sd = pd.read_csv('../data/Twitter-sentiment-self-drive-DFE.csv', encoding='latin1')
@@ -14,7 +23,9 @@ df = pd.concat([df_sd['text'], df_sd['sentiment']], axis=1, keys=['text', 'senti
 
 # normalize the sentiment values ({1; 2; 3; 4; 5} => {-1; 0; 1})
 df['sentiment'] = df['sentiment'].map({'1':-1, '2':-1, '3':0, '4':1, '5':1})
-del df_sd
+df_sd = df
+
+print('self-drive loaded')
 
 # text-emotion
 df_te = pd.read_csv('../data/text_emotion.csv')
@@ -40,7 +51,8 @@ df_te['sentiment'] = df_te['sentiment'].map({
 
 # append the rows to the main dataframe
 df = df.append(df_te)
-del df_te
+# del df_te
+print('text-emotion loaded')
 
 # apple
 df_ap = pd.read_csv('../data/Apple-Twitter-Sentiment-DFE.csv', encoding='latin1')
@@ -54,7 +66,8 @@ df_ap['sentiment'] = df_ap['sentiment'].map({'1':-1, '3':0, '5':1})
 
 # append the rows to the main dataframe
 df = df.append(df_ap)
-del df_ap
+# del df_ap
+print('apple loaded')
 
 # airline
 df_ai = pd.read_csv('../data/Airline-Sentiment-2-w-AA.csv', encoding='latin1')
@@ -65,7 +78,15 @@ df_ai['sentiment'] = df_ai['sentiment'].map({'negative':-1, 'neutral':0, 'positi
 
 # append the rows to the main dataframe
 df = df.append(df_ai)
-del df_ai
+# del df_ai
+print('airline loaded')
+
+df['sentiment'] = df['sentiment'].map({-1:1, 0:3, 1:5})
+
+SAMPLE_SIZE = 18144 # Number of neutral occurences
+df = df.loc[df['sentiment'] == 5].sample(SAMPLE_SIZE).append(df.loc[df['sentiment'] == 3].sample(SAMPLE_SIZE)).append(df.loc[df['sentiment'] == 1].sample(SAMPLE_SIZE))
+df = shuffle(df)
+print('dataframe shuffled, filtered. READY')
 
 # Array of vectorizers for the feature extraction step
 vecs = [
@@ -87,20 +108,52 @@ vecs = [
     TfidfVectorizer(stop_words=None, ngram_range=(2, 2), lowercase=False)
 ]
 
-def feat(df, vec):
-    return vec.fit_transform(df['text'].as_matrix()).toarray(), df['sentiment'].as_matrix()
+# dfs = [df_sd[:200], df_te, df_ap, df_ai, df]
+dfs = [df_sd[:200]]
 
+results_svm = []
+results_nb = []
 
-SEED = 17
-results = []
+print('LOOP DF START')
+for i in range(len(dfs)):
+    print('Loop df numero : {}'.format(i))
+    results_df_svm = []
+    results_df_nb = []
 
-df = df.sample(n=1000)
+    print('LOOP VEC START')
+    for idx, vec in enumerate(vecs):
+        print('Loop vec numero : {}'.format(idx))
+        print('======== LOOP NUMBER {} ========'.format(i*len(vecs) + idx))
+        current_vec = vec
+        X, y = current_vec.fit_transform(dfs[i]['text'].as_matrix()).toarray(), dfs[i]['sentiment'].as_matrix()
+        parameters = {'kernel':('linear', 'rbf'), 'C':[0.01, 0.05, 0.1, 0.5, 1, 5]}
 
+        # SVM
+        print('SVM start')
+        svr = svm.SVC()
+        clf = GridSearchCV(svr, parameters)
+        clf.fit(X, y)
+        results_df_svm.append(clf.cv_results_)
+        print('SVM finished')
 
-for idx, vec in enumerate(vecs):
-    current_vec = vec
-    X, y = current_vec.fit_transform(df['text'].as_matrix()).toarray(), df['sentiment'].as_matrix()
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=SEED)
+        # Naive Bayes
+        print('NB start')
+        gnb = MultinomialNB()
+        threshold = int(0.25*len(X))
+        y_pred = gnb.fit(X[:threshold], y[:threshold]).predict(X[threshold:])
+        target_names = ['negative', 'neutral', 'positive']
+        results_df_nb.append(classification_report(y[threshold:], y_pred, target_names=target_names))
+        print('NB finished')
 
+    results_svm.append(results_df_svm)
+    results_nb.append(results_df_nb)
+    print('LOOP VEC FINISHED')
 
-print(X_train)
+print('LOOP DF FINISHED')
+
+print('Pickle started')
+pickle.dump(results_svm, open( "results_svm.p", "wb" ) )
+pickle.dump(results_nb, open( "results_nb.p", "wb" ) )
+print('Pickle finished')
+
+print('PROGRAM FINISHED')
